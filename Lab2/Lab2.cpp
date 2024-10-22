022aedee6e13bc3ae3360636fc91340352fd33cd
GuI3Le <60442125+GuI3Le@users.noreply.github.com>
2024-10-22 18:56:15 +0200

    wszystkie zadania z lab2

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 
-//void Zad1() {
-//    // 27 = 16 + 8 +2 +1=a<<4 + a<<3 + a<<2 +a<<1
-//    unsigned char a = 255;
-//    uint16_t y = 27 * a;
-//    printf("C: DEC : %d\t HEX : %x\n", y, y);
-//    y = 0;
-//    _asm {
-//        xor al, al
-//        xor bl, bl
-//        mov al, a
-//        mov bl, a
-//        sal al, 4
-//        sal bl, 3
-//        add al, bl
-//        mov bl, a
-//        sax bl, 1
-//        add al, bl
-//        mov bl, a
-//        add al, bl
-//        mov y, al
-//    }
-//    printf("Assemler: DEC : %d\t HEX : %x\n", y, y);
-//}
+void Zad1() {
+    // 27 = 16 + 8 +2 +1=a<<4 + a<<3 +a<<1+a<<1
+    unsigned char a = 255;
+    uint16_t y = 27 * a;
+    printf("C: DEC : %d\t HEX : %x\n", y, y);
+    y = 0;
+    _asm {
+        xor ax, ax
+        xor bx, bx
+        mov al, a
+        mov bl, a
+        sal ax, 4
+        sal bx, 3
+        add ax, bx
+        xor bx, bx
+        mov bl, a
+        sal bx, 1
+        add ax, bx
+        xor bx, bx
+        mov bl, a
+        add ax, bx
+        mov y, ax
+    }
+    printf("Assemler: DEC : %d\t HEX : %x\n", y, y);
+}
 
 void Zad2() {
-    unsigned char a = 0x0;
-    uint8_t y = 0;
+    unsigned char a = 0x32;
+    uint16_t y = 0;
     if (a == 0x32) y = 0;
     else y = 1;
     printf("C: DEC : %d\t HEX : %x\n", y, y);
@@ -60,16 +62,14 @@ void Zad3() {
     printf("C: DEC : %d\t HEX : %x\n", y, y);
     _asm {
         xor al, al
-        mov al, 0x32
+        mov al, a
         cmp al, 0x32
-        jnz et1 //ZF=0
-        jz et2 //ZF=1
-        et1 :
-        mov y, 1
-            jmp koniec
-            et2 :
+        jnz et
         mov y, 0
-            koniec :
+        jmp koniec
+        et :
+            mov y, 1
+        koniec :
     }
     printf("Assembler: DEC : %d\t HEX : %x\n", y, y);
 }
@@ -79,7 +79,6 @@ void Zad4() {
     uint8_t y = 0;
     if (a == 0x32) {
         y = 0;
-
     }
     else {
         y = 1;
@@ -87,16 +86,15 @@ void Zad4() {
     printf("C: DEC : %d\t HEX : %x\n", y, y);
     _asm {
         xor al, al
-        mov al, 0x32
-        add al, 0xC3
-        jnz et1 //ZF=0
-        jz et2 //ZF=1
-        et1 :
-        mov y, 1
-            jmp koniec
-            et2 :
+        mov al, a
+        add al, -0x32
+        jnz et
         mov y, 0
-            koniec :
+        jmp koniec
+        et :
+            mov y, 1
+        koniec :
+        
     }
     printf("Assembler: DEC : %d\t HEX : %x\n", y, y);
 
@@ -104,7 +102,7 @@ void Zad4() {
 
 void Zad5() {
     unsigned char a = 5;
-    unsigned char b = 7;
+    unsigned char b = 8;
     uint8_t y = 0;
     if (a == 5 && b == 7) {
         y = 1;
@@ -112,15 +110,30 @@ void Zad5() {
     else {
         y = 0;
     }
+    printf("C: DEC : %d\t HEX : %x\n", y, y);
+
     _asm {
         xor al, al
         xor bl, bl
         mov al, a
         mov bl, b
         sub al, 5
-        sub bl, 7
+        jz et1
+        jmp alt
+        et1:
+            sub bl, 7
+            jz et2
+            jmp alt
+            et2:
+                mov y, 1
+                jmp end
+                alt:
+                    mov y,0
+                end:
 
     }
+    printf("Assembler: DEC : %d\t HEX : %x\n", y, y);
+
 }
 
 int main()
